ac03e22046b8aa15be262203b543e5a82c960f1f
Aryan Bansal <aryanbansalpvt@gmail.com>
2025-11-07 15:55:38 +0530

    chat system

--- models/Chat.model.js
+++ models/Chat.model.js
@@ -18,5 +18,5 @@ const chatSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Chat = mongoose.models.Chat || mongoose.model("Chat", chatSchema);
+const Chat = mongoose.model("Chat", chatSchema);
 export default Chat;
